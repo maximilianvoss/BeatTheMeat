3fc6ca0f56d04ee91af840cd1550d1549fd99312
Maximilian Voss <maximilian.voss@gmail.com>
2018-05-13 15:56:53 +0200

    Refactoring: code optimizations

--- app/src/main/java/rocks/voss/beatthemeat/threads/JsonDownloadThread.java
+++ app/src/main/java/rocks/voss/beatthemeat/threads/JsonDownloadThread.java
@@ -43,6 +43,12 @@ public class JsonDownloadThread extends Thread {
         urls.add(url);
     }
 
+    public void addUrls(URL... urls) {
+        for (URL url : urls) {
+            addUrl(url);
+        }
+    }
+
     public void addUrls(String... urls) throws MalformedURLException {
         for (String url : urls) {
             addUrl(url);
