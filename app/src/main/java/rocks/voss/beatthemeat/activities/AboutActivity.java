2f4d8cb4fade859e98c502d9d41ff84160546fb1
Maximilian Voss <maximilian.voss@gmail.com>
2018-06-03 20:26:51 +0200

    About: Finishing the About part

--- app/src/main/java/rocks/voss/beatthemeat/activities/AboutActivity.java
+++ app/src/main/java/rocks/voss/beatthemeat/activities/AboutActivity.java
@@ -2,7 +2,10 @@ package rocks.voss.beatthemeat.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.widget.TextView;
 
+import rocks.voss.beatthemeat.BuildConfig;
 import rocks.voss.beatthemeat.R;
 
 public class AboutActivity extends Activity {
@@ -11,5 +14,11 @@ public class AboutActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView copyrightText = (TextView) findViewById(R.id.copyrighttext);
+        copyrightText.setMovementMethod(LinkMovementMethod.getInstance());
+        String text = copyrightText.getText().toString();
+        text = text.replace("Version: X", "Version : " + BuildConfig.VERSION_NAME);
+        copyrightText.setText(text);
     }
 }
